90fa5d9a0b5c7af79ed37e8aea9ac773a0ebecbc
xzh <13055237860@163.com>
2019-02-13 14:53:53 +0800

    update firstdemo 2

--- DaoHui/src/FirstDemo.java
+++ DaoHui/src/FirstDemo.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("daohui");
-        ArrayList<String>
+        //ArrayList<String>
+		System.out.println("aaa");
     }
 }
